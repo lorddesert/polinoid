048e1273afa25b5afb5766cc1dd9a7063c1e3918
lorddesert <sacha.nocetto@hotmail.com>
2024-01-05 14:26:07 -0300

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Tab from './Tab.vue';
+
+</script>
+<template>
+	<header>
+		<nav>
+			<ul>
+				<li>
+					<Tab to="/">Kanban</Tab>
+				</li>
+				<li>
+					<Tab to="/notes">Notes</Tab>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<style scoped>
+
+	header {
+		display: grid;	
+		place-items: center;
+	}
+
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		gap: .5rem;
+	}
+
+
+
+</style>
