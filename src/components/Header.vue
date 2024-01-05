a21e5e16c3ce29869c575644cfdb4cb3a52f1687
lorddesert <sacha.nocetto@hotmail.com>
2024-01-05 14:41:17 -0300

    remove Tab component and put router-link instead

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,40 @@
 <script setup lang="ts">
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import Tab from './Tab.vue';
 
 </script>
 <template>
 	<header>
 		<nav>
-			<ul>
-				<li>
-					<Tab to="/">Kanban</Tab>
-				</li>
-				<li>
-					<Tab to="/notes">Notes</Tab>
-				</li>
-			</ul>
+			<Tabs default-value="/">
+				<TabsList>
+					<router-link to="/">
+						<TabsTrigger value="/">
+							Kanban
+						</TabsTrigger>
+					</router-link>
+					<router-link to="/notes">
+						<TabsTrigger value="/notes">
+							Notes
+						</TabsTrigger>
+					</router-link>
+				</TabsList>
+			</Tabs>
 		</nav>
 	</header>
 </template>
 
 <style scoped>
+header {
+	display: grid;
+	place-items: center;
+}
 
-	header {
-		display: grid;	
-		place-items: center;
-	}
-
-	ul {
-		list-style-type: none;
-		margin: 0;
-		padding: 0;
-		display: flex;
-		gap: .5rem;
-	}
-
-
-
+ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	gap: .5rem;
+}
 </style>
