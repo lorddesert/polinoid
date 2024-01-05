d800d62f9360239f3d671cdd60b805283a514002
lorddesert <sacha.nocetto@hotmail.com>
2024-01-05 14:26:01 -0300

    add vue router and shadcn ui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
-import { createApp } from "vue";
-import "./styles.css";
-import App from "./App.vue";
+import "./assets/index.css"
+// import "./styles.css";
 
-createApp(App).mount("#app");
+import { createApp } from "vue";
+import App from "./App.vue";
+import Kanban from './pages/Kanban.vue'
+import Notes from './pages/Notes.vue'
+
+import { createRouter, createWebHashHistory } from "vue-router"
+
+const routes = [
+  {path: '/', component: Kanban},
+  {path: '/notes', component: Notes},
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+app.mount("#app");
