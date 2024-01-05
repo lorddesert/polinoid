cb7afa3b6e0cc8f275bcc25410fa67bc48692512
lorddesert <sacha.nocetto@hotmail.com>
2024-01-05 14:25:02 -0300

    add card types

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,11 @@ export async function openDatabaseConnection() {
 
   return await SQLite.open(dbPath)
 }
+
+export type CardStatus = ["backlog", "in progres", "done"]
+
+export type Card = {
+  title: string,
+  description: string,
+  status: CardStatus
+}
