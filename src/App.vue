7bbe823e574fbebd65a5c4becbbec46b1a279cb1
lorddesert <sacha.nocetto@hotmail.com>
2024-01-05 21:13:26 -0300

    add toaster

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Header from './components/Header.vue'
-
+import Toaster from '@/components/ui/toast/Toaster.vue'
 </script>
 
 <template>
-  <div>
-    <Header />
-
-    <router-view></router-view>
-  </div>
+  <Header />
+  
+  <router-view></router-view>
+  <Toaster class=" absolute" />
 </template>
