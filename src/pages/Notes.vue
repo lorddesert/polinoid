db53a59c8af8ee2c4bed0a64befc9f15e0b66767
lorddesert <sacha.nocetto@hotmail.com>
2024-01-05 21:13:21 -0300

    add dialog and toast to form

--- src/pages/Notes.vue
+++ src/pages/Notes.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import { Textarea } from "@/components/ui/textarea"
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { ref } from 'vue';
+import { Input } from '@/components/ui/input'
+import { useToast } from '@/components/ui/toast/use-toast'
 const notes = ref([
   {
     id: 1,
@@ -20,6 +32,10 @@ const notes = ref([
 const currentNoteID = ref(-1)
 const newBody = ref("")
 const dumbText = ref("Dumb")
+const noteTitle = ref("")
+
+const { toast } = useToast()
+
 function writeDumbText(e: any) {
   newBody.value = e.target.value
 }
@@ -28,6 +44,7 @@ function selectNote(id: number) {
   const selectedNote = notes.value.find(note => note.id === id)
 
   newBody.value = selectedNote!.body
+  noteTitle.value = selectedNote!.title
 
   currentNoteID.value = selectedNote!.id
 }
@@ -47,11 +64,16 @@ function saveNote() {
   })
 
   notes.value = newNotes
+
+  toast({
+    title: 'Note saved!',
+    // description: `Note : ${selectedNote!.title} has been saved!`,
+  });
 }
 </script>
 
 <template>
-  <Card>
+  <Card class="min-h-96">
     <CardHeader>
       <CardTitle>Notes</CardTitle>
 
@@ -60,14 +82,47 @@ function saveNote() {
       <section>
         <ul class="grid gap-1 px-1">
           <Card class=" min-w-48" v-for="note in notes">
-            <Button @click="selectNote(note.id)" class="block w-full text-left" variant="ghost">{{ note.title }}</Button>
+            <Button @click="selectNote(note.id)" :class="note.id === currentNoteID && 'border border-slate-500'"
+              class="block w-full text-left" variant="ghost">{{ note.title }}</Button>
           </Card>
         </ul>
       </section>
-      <section>
+      <section v-if="currentNoteID !== -1">
 
-        <Textarea name="note" id="note" cols="30" rows="10" @input="writeDumbText" v-model="newBody"></Textarea>
-        <Button @click="saveNote" class=" my-2 bg-teal-600 hover:bg-teal-700 mr-4">Save Note</Button>
+        <Card>
+          <CardHeader class="py-2">
+            <Input class=" border-t-0 border-l-0 border-r-0 rounded-none" v-model="noteTitle" />
+            <!-- <CardTitle contenteditable spellcheck="false" >
+              {{ notes?.find(note => note.id === currentNoteID)?.title || '' }}
+            </CardTitle> -->
+          </CardHeader>
+          <Textarea class=" rounded-t-none border-none outline-none" name="note" id="note" cols="30" rows="10"
+            @input="writeDumbText" v-model="newBody"></Textarea>
+        </Card>
+        <div class="my-2 flex gap-1">
+          <Button @click="saveNote" class="bg-teal-600 hover:bg-teal-700">Save Note</Button>
+          <Dialog>
+            <DialogTrigger>
+              <Button @click="" variant="destructive">Delete</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Delete note</DialogTitle>
+                <DialogDescription>
+                  Are you sure you want to DELETE that note?
+                </DialogDescription>
+              </DialogHeader>
+
+              <DialogFooter>
+                <DialogClose>
+                  <Button @click="" variant="secondary">Cancel</Button>
+                </DialogClose>
+                <Button @click="" variant="destructive">Delete</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+
         <Alert class="my-2">
           <AlertTitle>Dumb text:</AlertTitle>
           <AlertDescription>{{ dumbText }}</AlertDescription>
