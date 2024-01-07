6e812e31aef57c49c3ca3da42952a56b78ec20bc
lorddesert <sacha.nocetto@hotmail.com>
2024-01-06 21:40:59 -0300

    feat: add CRUD

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -6,12 +6,73 @@ type Todo = {
   body: string
 }
 
+export type { Todo }
+
 export class TodoModel {
   async getAll() {
     const db = await openDatabaseConnection()
-    const allTodos =  await db.select("select * from todo;")
+    let allTodos: Todo[] = []
+    try {
+      allTodos = await db.select("select * from todo;")
+    } catch (e) {
+      console.log('Error trying to fetch all todos:')
+      console.log(e)
+    }
 
     db.close()
     return allTodos
+  }
+
+  async getFilteredTodos(
+    {
+      limit,
+      order = 'ASC',
+      orderColumn = 'id'
+    }: {
+      limit?: number | undefined,
+      order?: 'ASC' | 'DESC',
+      orderColumn?: 'id' | 'title' | 'body'
+    }) {
+    const db = await openDatabaseConnection(true)
+    try {
+      const filteredTodos: Todo[] = await db.select(`select * from todo order by ${orderColumn} ${order} ${limit && `limit ${limit}`} ;`)
+
+      db.close()
+      return filteredTodos
+    } catch (e) {
+      console.log('Error trying to fetch filtered todos:')
+      console.log(e)
+      db.close()
+    }
+  }
+
+  async modifyTodo(todo: Todo) {
+    const db = await openDatabaseConnection()
+    try {
+      await db.execute(`update todo set title = ${todo.title} where id = ${todo.id};`)
+      await db.close()
+    } catch (e) {
+      console.log(`Error trying to modify todo: `)
+      console.log(e)
+    }
+
+    db.close()
+  }
+
+  async deleteTodo(todo: Todo) {
+    const db = await openDatabaseConnection()
+
+    db.execute(`delete from todo where id = ${todo.id};`)
+
+    db.close()
+  }
+
+
+  async createTodo(todo: Todo) {
+    const db = await openDatabaseConnection()
+
+    await db.execute(`insert into todo (title, body) values (${todo.title},${todo.body} );`)
+
+    db.close()
   }
 }
