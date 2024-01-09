03c04104a19516de550b47bbacf1df2f5fdc9fd9
lorddesert <sacha.nocetto@hotmail.com>
2024-01-09 16:17:21 -0300

    feat: componetize and add functions to create and delete notes in local

--- src/pages/Notes.vue
+++ src/pages/Notes.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  DialogClose,
-} from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Textarea } from "@/components/ui/textarea"
 import { ref } from 'vue';
 import { Input } from '@/components/ui/input'
 import { useToast } from '@/components/ui/toast/use-toast'
+import CreateNoteButton from './CreateNoteButton.vue'
+import DeleteNoteDialog from './DeleteNoteDialog.vue'
+// import { Note as NoteType } from "@/models/note"
+
+// mock production DATA
 const notes = ref([
   {
     id: 1,
     title: 'asd 1',
-    body: 'Body 1'
+    body: 'Body 1',
+    draft: false
   },
   {
     id: 2,
     title: 'asd 21',
-    body: 'Body 2'
+    body: 'Body 2',
+    draft: false
   },
 ])
 
@@ -47,6 +44,10 @@ function selectNote(id: number) {
   currentNoteID.value = selectedNote!.id
 }
 
+function deselectNote() {
+  currentNoteID.value = -1
+}
+
 function saveNote() {
   const newNotes = notes.value.map(note => {
     if (note.id === currentNoteID.value) {
@@ -67,6 +68,30 @@ function saveNote() {
     // description: `Note : ${selectedNote!.title} has been saved!`,
   });
 }
+
+async function createNote() {
+  const newGenericNote = {
+    // ID is just for FE, BE will create it's real ID on save.
+    id: notes.value.at(-1)!.id + 1,
+    title: 'New note',
+    body: 'Your awesome note!',
+    draft: true
+  }
+
+  notes.value = [...notes.value, newGenericNote]
+
+  selectNote(newGenericNote.id)
+  // For production?
+  // const model = new NoteModel()
+  // await model.createNote(newGenericNote)
+}
+
+async function deleteNote() {
+  const newNotes = notes.value.filter(note => note.id !== currentNoteID.value)
+
+  notes.value = newNotes
+  deselectNote()
+}
 </script>
 
 <template class="">
@@ -78,10 +103,7 @@ function saveNote() {
     <div class="notes">
       <section>
         <ul class="grid gap-1 px-1">
-          <Button @click="" variant="outline" class=" rounded-b-none">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-circle w-6 h-6 mr-2"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
-            <!-- Create new -->
-          </Button>
+          <CreateNoteButton :createNote="createNote" />
           <Card class=" min-w-48" v-for="note in notes">
             <Button @click="selectNote(note.id)" :class="note.id === currentNoteID && 'border border-slate-500'"
               class="block w-full text-left" variant="ghost">{{ note.title }}</Button>
@@ -102,26 +124,7 @@ function saveNote() {
         </Card>
         <div class="my-2 flex gap-1">
           <Button @click="saveNote" class="bg-teal-600 hover:bg-teal-700">Save Note</Button>
-          <Dialog>
-            <DialogTrigger>
-              <Button @click="" variant="destructive">Delete</Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Delete note</DialogTitle>
-                <DialogDescription>
-                  Are you sure you want to DELETE that note?
-                </DialogDescription>
-              </DialogHeader>
-
-              <DialogFooter>
-                <DialogClose>
-                  <Button @click="" variant="secondary">Cancel</Button>
-                </DialogClose>
-                <Button @click="" variant="destructive">Delete</Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+          <DeleteNoteDialog :deleteNote="deleteNote" />
         </div>
       </section>
       <section v-else>
@@ -129,7 +132,11 @@ function saveNote() {
           <CardHeader>
             <CardTitle class="text-lg">Notes editing ✨</CardTitle>
             <CardDescription class="flex items-center gap-2">
-              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
+                  fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+              </svg>
               <span>Click a note to start editing!</span>
             </CardDescription>
           </CardHeader>
@@ -152,5 +159,4 @@ function saveNote() {
   display: grid;
   grid-template-columns: auto 1fr;
 
-}
-</style>
+}</style>
