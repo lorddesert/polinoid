5894348affa944dd825f597753b37786ac2551e6
lorddesert <sacha.nocetto@hotmail.com>
2024-01-09 16:17:06 -0300

    fix: assign createNote function to on click event

--- src/pages/CreateNoteButton.vue
+++ src/pages/CreateNoteButton.vue
@@ -7,7 +7,7 @@
 </script>
 
 <template>
-	<Button @click="" variant="outline" class=" rounded-b-none" id="add-new-note-button">
+	<Button @click="createNote" variant="outline" class=" rounded-b-none" id="add-new-note-button">
 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
 			stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-circle w-6 h-6 mr-2">
 			<circle cx="12" cy="12" r="10" />
