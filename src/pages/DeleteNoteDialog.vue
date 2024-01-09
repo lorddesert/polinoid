c3819453b71922e61114d9b64ffa449c686b9b1c
lorddesert <sacha.nocetto@hotmail.com>
2024-01-09 16:16:31 -0300

    feat: componetize delete note dialog

---
+++ src/pages/DeleteNoteDialog.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+	deleteNote: () => Promise<void>;
+}>()
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button';
+
+</script>
+
+<template>
+	<Dialog>
+		<DialogTrigger>
+			<Button @click="" variant="destructive">Delete</Button>
+		</DialogTrigger>
+		<DialogContent>
+			<DialogHeader>
+				<DialogTitle>Delete note</DialogTitle>
+				<DialogDescription>
+					Are you sure you want to DELETE that note?
+				</DialogDescription>
+			</DialogHeader>
+
+			<DialogFooter>
+				<DialogClose>Cancel</DialogClose>
+				<DialogClose>
+					<Button @click="deleteNote" variant="destructive">Delete</Button>
+				</DialogClose>
+			</DialogFooter>
+		</DialogContent>
+	</Dialog>
+</template>
