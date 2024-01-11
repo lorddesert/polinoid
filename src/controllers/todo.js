8cc13ef74ffb82b627354d2e97435abdbf7476b9
lorddesert <sacha.nocetto@hotmail.com>
2024-01-11 18:51:11 -0300

    feat: use todo model

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -2,10 +2,18 @@ import { TodoModel } from '../models/todo'
 
 export class TodoController {
   static async getAll() {
-    const todoModel = new TodoModel()
-
-    const allTodos = await todoModel.getAll()
-
-    return allTodos
+    return await TodoModel.getAll()
+  }
+  static async getFilteredTodos({ limit, order, orderColumn }) {
+    return await TodoModel.getFilteredTodos({ limit, order, orderColumn })
+  }
+  static async modifyTodo(todo) {
+    return await TodoModel.modifyTodo(todo)
+  }
+  static async createTodo(todo) {
+    return await TodoModel.createTodo(todo)
+  }
+  static async deleteTodo(todo) {
+    return await TodoModel.deleteTodo(todo)
   }
 }
