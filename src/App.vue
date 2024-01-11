545326fb346f1d4ad93c2d38b11e42fc3eec6ce8
lorddesert <sacha.nocetto@hotmail.com>
2024-01-11 20:17:15 -0300

    feat: add state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script lang="ts" setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { Ref, onMounted, ref } from 'vue';
 import Header from './components/Header.vue'
 import Toaster from '@/components/ui/toast/Toaster.vue'
+import { TodoController } from '@/controllers/todo.ts'
+import { NoteController } from '@/controllers/note.ts'
+import { Note } from './models/note';
+import { Todo } from './models/todo';
+
+const notes: Ref<Note[]> = ref([])
+const todos: Ref<Todo[]> = ref([])
+
+onMounted(async () => {
+  const allTodos = await TodoController.getAll()
+  const allNotes = await NoteController.getAll()
+
+  todos.value = allTodos
+  notes.value = allNotes
+
+  console.log(allTodos, allNotes)
+})
 </script>
+
 
 <template>
   <Header />
