b9d15d0fc8e6dc1c60d81570ff30e948e0fd8ff0
lorddesert <sacha.nocetto@hotmail.com>
2024-01-11 19:36:48 -0300

    feat: update note controller

--- src/controllers/note.js
+++ src/controllers/note.js
@@ -2,6 +2,26 @@ import { NoteModel } from '../models/note'
 
 export class NoteController {
   static async getAll() {
-    return await NoteModel
+    return await NoteModel.getAll()
+  }
+
+  static async createNote(note) {
+    return await NoteModel.createNote(note)
+  }
+
+  static async deleteNote(note) {
+    return await NoteModel.deleteNote(note)
+  }
+
+  static async updateNote(note) {
+    return await NoteModel.modifyNote()
+  }
+
+  static async getFilteredNotes({
+    limit = 1,
+    order = 'ASC',
+    orderColumn = 'id'
+  }) {
+    return await NoteModel.getFilteredNotes({ limit, order, orderColumn })
   }
 }
