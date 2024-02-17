57c08cf4c772326b3eaf6db8e80ee49546f932d8
lorddesert <sacha.nocetto@hotmail.com>
2024-02-17 20:47:13 -0300

    format

--- src/components/StatusSelect.vue
+++ src/components/StatusSelect.vue
@@ -25,7 +25,7 @@
   <label for="status" class="block text-sm font-medium text-gray-900 dark:text-white">Move to</label>
   <!-- <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> -->
   <select @change="moveTodo" id="status" class=" flex h-10 items-center justify-between rounded-md border border-input bg-background text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1">
-    <option :disabled="status === KANBAN_STATUS_OPTIONS.BACKLOG" value="backlog" :selected="status === KANBAN_STATUS_OPTIONS.BACKLOG">Backlog</option>
+    <option value="backlog" :disabled="status === KANBAN_STATUS_OPTIONS.BACKLOG" :selected="status === KANBAN_STATUS_OPTIONS.BACKLOG">Backlog</option>
     <option value="wip" :disabled="status === KANBAN_STATUS_OPTIONS.WORK_IN_PROGRESS" :selected="status === KANBAN_STATUS_OPTIONS.WORK_IN_PROGRESS">Work in progress</option>
     <option value="done" :disabled="status === KANBAN_STATUS_OPTIONS.DONE" :selected="status === KANBAN_STATUS_OPTIONS.DONE">Done</option>
   </select>
