9ad789d4a5b1b1f26cf06cc293fefb8e4e523e52
lorddesert <sacha.nocetto@hotmail.com>
2024-02-17 18:40:24 -0300

    feat: add the correct type and add the status select

--- src/components/KanbanCard.vue
+++ src/components/KanbanCard.vue
@@ -1,14 +1,16 @@
 <script setup lang=ts>
-	defineProps<{
-		card: {
-			title: string,
-			description: string
-		}
-	}>()
+import { Card } from '@/components/ui/card';
+import StatusSelect from './StatusSelect.vue';
+import { Todo } from '@/models/todo';
+defineProps<{
+	card: Todo
+}>()
+
 </script>
 
 <template>
-	<Card class="p-2">
+	<Card class="p-2 gap-4">
+		<StatusSelect :status="card.status" />
 		<h2 class="text-base">{{ card.title }}</h2>
 		<p class="text-sm text-slate-600">{{ card.description }}</p>
 	</Card>
