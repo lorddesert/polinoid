7a7111f02a468924c0133babf45cb5e81168867b
lorddesert <sacha.nocetto@hotmail.com>
2024-02-17 21:23:44 -0300

    feat: change todos ref to be a computed read only constant

--- src/state/pages/kanban.ts
+++ src/state/pages/kanban.ts
@@ -1,26 +1,29 @@
 import { TodoController } from '@/controllers/todo';
 import { Todo } from '@/models/todo';
-import { ref, Ref } from 'vue';
+import { computed, ref, Ref } from 'vue';
 
 export const unfilteredAllTodos: Ref<Todo[]> = ref([])
 export const dataHasLoaded = ref(false)
-export const todos: Ref<{
-  backlog: Todo[],
-  wip: Todo[],
-  done: Todo[]
-}> = ref({
-  backlog: [],
-  wip: [],
-  done: []
-})
+export const todos = computed(() => {
+    const newTodos: {
+        backlog: Todo[],
+        wip: Todo[],
+        done: Todo[]
+      } = {
+        backlog: [],
+        wip: [],
+        done: []
+      }
+
+    unfilteredAllTodos.value.forEach(todo => newTodos[todo.status].push(todo))
+
+    console.log(newTodos)
+    return newTodos
+  })
+  
+
 
 export async function fetchKanbanData() {
-  const newTodos = todos.value
   unfilteredAllTodos.value = await TodoController.getAll()
-  unfilteredAllTodos.value.forEach(todo => newTodos[todo.status].push(todo))
-
-  todos.value = newTodos
   dataHasLoaded.value = true
 }
-
-
