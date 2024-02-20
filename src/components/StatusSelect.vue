979ac64b78f8424c1d293d5fc0658d74c2a6fcc1
lorddesert <sacha.nocetto@hotmail.com>
2024-02-20 16:55:40 -0300

    feat: add proper select element

--- src/components/StatusSelect.vue
+++ src/components/StatusSelect.vue
@@ -1,33 +1,70 @@
 <script setup lang="ts">
+import { TodoController } from '@/controllers/todo';
 import { unfilteredAllTodos } from '@/state/pages/kanban';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { ref, watch } from 'vue';
   const props = defineProps<{
     status: string,
     cardID: any,
+    isHoveringCard: boolean
   }>()
+
+  const selectValue = ref(props.status)
+
+  watch(selectValue, (prev, current) => {
+    if (!current) return
+    if (prev === current) return
+    
+    moveTodo(selectValue.value)
+  })
   
 
-  function moveTodo(e: any) {
-    const newStatus: 'wip' | 'done' | 'backlog' = e.target.value
-
+  function moveTodo(newStatus: 'wip' | 'done' | 'backlog') {
     if (newStatus === props.status) return false
+
+    console.log(newStatus)
 
     // recover the ID of the card that has changed
     // change the value on local of the status
     // change the value of the status on the server
 
+    // WOrks on local, to see the server.
+    // Server: Done
+
     const selectedTodo = unfilteredAllTodos.value.find(todo => todo.id === props.cardID)
     selectedTodo!.status = newStatus
+
+    // console.log(selectedTodo)
+    // return
+    TodoController.modifyTodo(selectedTodo)
   }
-  
 </script>
 <template>
-<form class="flex max-w-sm mx-auto items-center gap-4">
-  <label for="status" class="block text-sm font-medium text-gray-900 dark:text-white">Move to</label>
-  <!-- <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> -->
-  <select @change="moveTodo" id="status" class=" flex h-10 items-center justify-between rounded-md border border-input bg-background text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1">
-    <option value="backlog">Backlog</option>
-    <option value="wip">Work in progress</option>
-    <option value="done">Done</option>
-  </select>
-</form>
+  <Select v-model="selectValue" :default-value="props.status">
+    <SelectTrigger class=" w-min">
+      <SelectValue placeholder="Move to" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectLabel>Status</SelectLabel>
+        <SelectItem value="backlog">
+          Backlog
+        </SelectItem>
+        <SelectItem value="wip">
+          Work in progress
+        </SelectItem>
+        <SelectItem value="done">
+          Done
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
 </template>
