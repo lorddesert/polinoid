8011997f1ccf07b3ed548246ddba5b8280ba2fe3
lorddesert <sacha.nocetto@hotmail.com>
2024-02-20 16:54:53 -0300

    fix: clean unused code

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,11 @@
 import { Ref, ref } from 'vue';
 import Header from './components/Header.vue'
 import Toaster from '@/components/ui/toast/Toaster.vue'
-// import { TodoController } from '@/controllers/todo.ts'
-// import { NoteController } from '@/controllers/note.ts'
 import { Note } from './models/note';
 import { Todo } from './models/todo';
 
 const notes: Ref<Note[]> = ref([])
 const todos: Ref<Todo[]> = ref([])
-
-// onMounted(() => {
-//   async function fetchInitialData() {
-//     const allTodos = await TodoController.getAll()
-//     const allNotes = await NoteController.getAll()
-
-//     todos.value = allTodos
-//     notes.value = allNotes
-
-//     console.log(allTodos, allNotes)
-//   }
-
-//   fetchInitialData()
-// })
 </script>
 
 
@@ -31,9 +15,4 @@ const todos: Ref<Todo[]> = ref([])
 
   <router-view :all-todos="notes" :all-notes="todos"></router-view>
   <Toaster class=" absolute" />
-  <pre>
-    <code>
-      {{ JSON.stringify(notes, null, 2) }}
-    </code>
-  </pre>
 </template>
