2ad4dfe68f737d5e1b7512d4b6fe256134edf4fd
lorddesert <sacha.nocetto@hotmail.com>
2024-02-20 16:52:52 -0300

    feat: encapsulate code in a try catch

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,15 +2,20 @@ import SQLite from 'tauri-plugin-sqlite-api'
 import { invoke } from '@tauri-apps/api/tauri'
 
 export async function openDatabaseConnection(envPath?: boolean) {
-  let dbPath = ''
+  try {
+    let dbPath = ''
 
-  if (envPath) {
-    dbPath = import.meta.env.DB_PATH
-    return await SQLite.open(dbPath)
+    if (envPath) {
+      dbPath = import.meta.env.DB_PATH
+      return await SQLite.open(dbPath)
+    }
+  
+    dbPath = await invoke('get_db_path')
+    return await SQLite.open(dbPath)    
+  } catch (error) {
+    console.log(error)
   }
 
-  dbPath = await invoke('get_db_path')
-  return await SQLite.open(dbPath)
 }
 type Card = {
   title: string,
