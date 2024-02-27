abe95e2f6608be4fc706b4c3d38cce678778a400
lorddesert <sacha.nocetto@hotmail.com>
2024-02-27 19:53:34 -0300

    fix: remove unused code

--- src/components/KanbanCard.vue
+++ src/components/KanbanCard.vue
@@ -1,6 +1,5 @@
 <script setup lang=ts>
 import { Card } from '@/components/ui/card';
-import { Button } from '@/components/ui/button'
 import StatusSelect from './StatusSelect.vue';
 import { Todo } from '@/models/todo';
 import { ref } from 'vue';
